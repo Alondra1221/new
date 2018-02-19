e7b679eb103b0d805e17144386039af06c18e9f5
Interware <interware@localhost.localdomain>
2018-02-19 16:21:11 -0600

    first commit

---
+++ src/app/core.clj
@@ -0,0 +1,30 @@
+(ns app.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(defn fibonacci [x]
+
+  if(<= x 2) 1
+  (+ (fibonacci (- x 2 ))(fibonacci (- x - 1 )))))
+
+  (defn factorial [x])
+  (if (<= x 1)1)
+  (* (fatorial (-x 1))) x)))
+
+  ;; ;(1) => (* 1 )
+  ;; ;(1) => (* 1 2 )
+  ;; ;(1) => (* 1 2 3 )
+  ;; ;(1) => (* 1 2 3 4)
+  ;; ;(1) => (* 1 2 3 4 5 )
+
+(defn factorial [x])
+
+(defn -main [& args]
+
+
+  (let [arg-1 (or(first args )"0")n (Interger/parseInt arg-1)]
+  (println "fib(" n")="(fibonacci)n)))
+  (println "!("n")=") (factorial n ))))
